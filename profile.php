39cf56b70a0c78ad0519de9b411ce9e18217ef87
andrionto <42750435+andrionto@users.noreply.github.com>
2018-08-28 03:07:47 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,196 @@
+<?php
+session_start();
+include_once 'includes/functions.php';
+$user = new User();
+$id = @$_SESSION['id'];
+if (!$user->get_session()){
+    header("location:login.php");
+}
+?>
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Data History Raspberry</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Minimal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!--<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />-->
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery.min.js"> </script>
+<script src="js/bootstrap.min.js"> </script>
+  
+<!-- Mainly scripts -->
+<script src="js/jquery.metisMenu.js"></script>
+<script src="js/jquery.slimscroll.min.js"></script>
+<!-- Custom and plugin javascript -->
+<link href="css/custom.css" rel="stylesheet">
+<script src="js/custom.js"></script>
+<script src="js/screenfull.js"></script>
+		<script>
+		$(function () {
+			$('#supported').text('Supported/allowed: ' + !!screenfull.enabled);
+
+			if (!screenfull.enabled) {
+				return false;
+			}
+
+			
+
+			$('#toggle').click(function () {
+				screenfull.toggle($('#container')[0]);
+			});
+			
+
+			
+		});
+		</script>
+
+
+
+</head>
+<body>
+<div id="wrapper">
+      <!----->
+        <nav class="navbar-default navbar-static-top" role="navigation">
+             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <h1> <a class="navbar-brand" href="index.html">Minimal</a></h1>         
+			   </div>
+			 <div class=" border-bottom">
+        	<div class="full-left">
+        	  <section class="full-top">
+				<button id="toggle"><i class="fa fa-arrows-alt"></i></button>	
+			</section>
+			<form class=" navbar-left-right">
+            
+            </form>
+            <div class="clearfix"> </div>
+           </div>
+     
+       
+            <!-- Brand and toggle get grouped for better mobile display -->
+		 
+		   <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="drop-men" >
+		        <ul class=" nav_1">
+
+					<li class="dropdown">
+		              <a href="#" class="dropdown-toggle dropdown-at" data-toggle="dropdown"><span class=" name-caret">User<i class="caret"></i></span><img src="images/wo.jpg"></a>
+		              <ul class="dropdown-menu " role="menu">
+		                <li><a href="profile.php"><i class="fa fa-user"></i>Edit Profile</a></li>
+		                <li><a href="calendar.php"><i class="fa fa-calendar"></i>Calender</a></li>
+                        <li><a href="index.php?q=logout"><i class="fa fa-sign-out"></i>Logout</a></li>
+		              </ul>
+		            </li>
+		           
+		        </ul>
+		     </div><!-- /.navbar-collapse -->
+			<div class="clearfix">
+       
+     </div>
+	  
+		    <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+				
+                    <li>
+                        <a href="index.php" class=" hvr-bounce-to-right"><i class="fa fa-dashboard nav_icon "></i><span class="nav-label">Home</span> </a>
+                    </li>
+                    
+                    <li>
+                        <a href="gallery.php" class=" hvr-bounce-to-right"><i class="fa fa-picture-o nav_icon"></i> <span class="nav-label">Gallery</span> </a>
+                    </li>
+                    
+                </ul>
+            </div>
+			</div>
+        </nav>
+		 <div id="page-wrapper" class="gray-bg dashbard-1">
+       <div class="content-main">
+ 
+ 	<!--banner-->	
+		    <div class="banner">
+		    	<h2>
+				<a href="index.php">Home</a>
+				<i class="fa fa-angle-right"></i>
+				<span>Profile</span>
+				</h2>
+		    </div>
+		<!--//banner-->
+ 	 <!--gallery-->
+ 	 <div class=" profile">
+
+		<div class="profile-bottom">
+			<h3><i class="fa fa-user"></i>Profile</h3>
+			<div class="profile-bottom-top">
+			<div class="col-md-4 profile-bottom-img">
+				<img src="images/pr.jpg" alt="">
+			</div>
+			<div class="col-md-8 profile-text">
+				<h6>Andri Gunawan</h6>
+				<table>
+				<tr><td>Department</td>  
+				<td>:</td>  
+				<td>Web Designer</td></tr>
+				
+				<tr>
+				<td>Email</td>
+				<td> :</td>
+				<td><a href="mudhant@gmail.com">mudhant@gmail.com</a></td>
+				</tr>
+				<tr>
+				<td>Country </td>
+				<td>:</td>
+				<td>Indonesia</td>
+				</tr>
+				</table>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+
+			<div class="profile-btn">
+
+                <button type="button" href="#" class="btn bg-red">Save changes</button>
+           <div class="clearfix"></div>
+			</div>
+			 
+			
+		</div>
+	</div>
+	<!--//gallery-->
+		<!---->
+<div class="copy">
+            <p> &copy; 2018. Andri gunawan | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>	    </div>
+		</div>
+		</div>
+		<div class="clearfix"> </div>
+       </div>
+     
+<!---->
+
+<!--scrolling js-->
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
+	<!--//scrolling js-->
+
+</body>
+</html>
+
+
+
---
+++ tanggal.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+    $tanggal=date("Y-m-d h-m-sa");
+echo $tanggal;
+?>
