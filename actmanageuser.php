c458895c27e5792ba3b005a7f15c4bbead007a77
andrionto <42750435+andrionto@users.noreply.github.com>
2018-08-28 02:57:47 +0700

    Add files via upload

---
+++ actmanageuser.php
@@ -0,0 +1,24 @@
+<?php
+include "includes/connection.php";
+
+$act=$_GET["act"];
+$id=$_POST["id_person"];
+$id_person=$_GET["id"];
+$nama=$_POST["nama"];
+$no_hp=$_POST["no_hp"];
+$status=$_POST["status"];
+if($act=="add"){
+	mysql_query("insert into no_person(nama, no_hp, status) values('$nama','$no_hp','1')") or die(mysql_error());
+	header('Location: kelola_user.php');
+}
+else if($act=="edit") {
+	mysql_query("update no_person set nama='$nama', no_hp='$no_hp', status='$status' where id='$id'") or die(mysql_error());
+	header('Location: kelola_user.php');
+}
+else if($act=="delete") {
+    //print_r($id_person);die();
+	mysql_query("update no_person set status = '0' where id='$id_person'") or die(mysql_error());
+	header('Location: kelola_user.php');
+}
+
+?>
